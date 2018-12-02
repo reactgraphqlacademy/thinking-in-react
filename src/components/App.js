e98c33be21e662751e70f9a0de9a78868d7b0a77
alexlbr <alex.lobera@gmail.com>
2018-12-02 19:11:06 +0000

    refactores setState(state,props => ...)

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,50 @@
-import React from 'react'
+import React from "react";
 
-import Menu from './Navigations/Menu'
-import Header from './Header'
-import books from '../mocks/books'
+import Menu from "./Navigations/Menu";
+import Header from "./Header";
+import books from "../mocks/books";
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
       books: books,
-      selectedFilter: 'All',
+      selectedFilter: "All",
       menu: { open: false }
-    }
+    };
   }
 
   toggleMenu = () => {
-    this.setState({ menu: { open: !this.state.menu.open } })
-  }
+    this.setState(state => ({
+      menu: { open: !state.menu.open }
+    }));
+  };
 
-  selectFilter = (filter) => {
+  selectFilter = filter => {
     this.setState({
       selectedFilter: filter,
-      books: filter === 'All' ? books : books.filter(book => (book.category === filter))
-    })
-  }
+      books:
+        filter === "All"
+          ? books
+          : books.filter(book => book.category === filter)
+    });
+  };
 
   render() {
-    const filters = [
-      'All',
-      'Web',
-      'Mobile',
-      'DevOps',
-      'Essentials',
-    ]
+    const filters = ["All", "Web", "Mobile", "DevOps", "Essentials"];
 
     const tabItems = filters.map(filter => (
-      <li className={filter === this.state.selectedFilter ? 'active' : ''} key={filter} onClick={() => this.selectFilter(filter)}>
+      <li
+        className={filter === this.state.selectedFilter ? "active" : ""}
+        key={filter}
+        onClick={() => this.selectFilter(filter)}
+      >
         <a href="#0">{filter}</a>
       </li>
-    ))
+    ));
 
     return (
       <div id="page-wrap">
-
         <Menu
           pageWrapId="page-wrap"
           isOpen={this.state.menu.open}
@@ -59,8 +61,11 @@ class App extends React.Component {
                 <a href="#about">About us</a>
               </li>
               <li>
-                <button onClick={this.toggleMenu} className="btn btn-lg btn-outline">
-                  <i className="fa fa-graduation-cap"></i> <span>Training</span>
+                <button
+                  onClick={this.toggleMenu}
+                  className="btn btn-lg btn-outline"
+                >
+                  <i className="fa fa-graduation-cap" /> <span>Training</span>
                 </button>
               </li>
             </ul>
@@ -79,9 +84,7 @@ class App extends React.Component {
             </div>
             <div className="row">
               <div className="col-lg-12">
-                <ul className="nav nav-pills text-center">
-                  {tabItems}
-                </ul>
+                <ul className="nav nav-pills text-center">{tabItems}</ul>
               </div>
             </div>
             <div className="row book-list">
@@ -106,15 +109,33 @@ class App extends React.Component {
             </div>
             <div className="row">
               <div className="col-lg-4 col-lg-offset-2">
-                <p>This library is an exercise for building UI in a <strong style={{ textDecoration: 'underline' }}>declarative way</strong>. This web site will help you understand the fundamental piece of ReactJS, components. You'll learn how to break an app in components (AKA componentization) and flow data accross them.</p>
+                <p>
+                  This library is an exercise for building UI in a{" "}
+                  <strong style={{ textDecoration: "underline" }}>
+                    declarative way
+                  </strong>
+                  . This web site will help you understand the fundamental piece
+                  of ReactJS, components. You'll learn how to break an app in
+                  components (AKA componentization) and flow data accross them.
+                </p>
               </div>
               <div className="col-lg-4">
-                <p>This ReactJS web site works, but it's not well implemented. The problem, the developer didn't think in React and there aren't many components. We challenge you to fork the repo an componentizise the app to unleash the power of ReactJS.</p>
+                <p>
+                  This ReactJS web site works, but it's not well implemented.
+                  The problem, the developer didn't think in React and there
+                  aren't many components. We challenge you to fork the repo an
+                  componentizise the app to unleash the power of ReactJS.
+                </p>
               </div>
               <div className="col-lg-8 col-lg-offset-2 text-center">
-                <a target="_blank" rel="noopener noreferrer" href="https://github.com/leanjscom/thinking-in-react" className="btn btn-lg btn-outline">
-                  <i className="fa fa-code-fork"></i> Fork me on Github
-                        </a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/leanjscom/thinking-in-react"
+                  className="btn btn-lg btn-outline"
+                >
+                  <i className="fa fa-code-fork" /> Fork me on Github
+                </a>
               </div>
             </div>
           </div>
@@ -136,13 +157,34 @@ class App extends React.Component {
                   <h3>Around the Web</h3>
                   <ul className="list-inline">
                     <li>
-                      <a target="_blank" rel="noopener noreferrer" href="https://github.com/leanjscom" className="btn-social btn-outline"><i className="fa fa-fw fa-github"></i></a>
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://github.com/leanjscom"
+                        className="btn-social btn-outline"
+                      >
+                        <i className="fa fa-fw fa-github" />
+                      </a>
                     </li>
                     <li>
-                      <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/leanjscom" className="btn-social btn-outline"><i className="fa fa-fw fa-twitter"></i></a>
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://twitter.com/leanjscom"
+                        className="btn-social btn-outline"
+                      >
+                        <i className="fa fa-fw fa-twitter" />
+                      </a>
                     </li>
                     <li>
-                      <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/leanjscom/" className="btn-social btn-outline"><i className="fa fa-fw fa-instagram"></i></a>
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://www.instagram.com/leanjscom/"
+                        className="btn-social btn-outline"
+                      >
+                        <i className="fa fa-fw fa-instagram" />
+                      </a>
                     </li>
                   </ul>
                 </div>
@@ -151,8 +193,9 @@ class App extends React.Component {
                   <p>
                     <a href="https://reactjs.academy/">ReactJS Academy</a>
                     <span>
-                      ReactJS Academy is Europes longest running dedicated React, Redux, and GraphQL training.
-                              </span>
+                      ReactJS Academy is Europes longest running dedicated
+                      React, Redux, and GraphQL training.
+                    </span>
                   </p>
                 </div>
               </div>
@@ -162,15 +205,22 @@ class App extends React.Component {
             <div className="container">
               <div className="row">
                 <div className="col-lg-12">
-                  <span>Copyright &copy;</span> <a href="https://leanjs.com/" target="_blank" rel="noopener noreferrer">LeanJS</a>
+                  <span>Copyright &copy;</span>{" "}
+                  <a
+                    href="https://leanjs.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    LeanJS
+                  </a>
                 </div>
               </div>
             </div>
           </div>
         </footer>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
