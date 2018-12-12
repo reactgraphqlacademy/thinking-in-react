26ae2e98f1c282465e2587b6bc65cb5eba857cae
Richard Moss <richard@englishtutorslive.com>
2018-12-12 21:32:07 +0000

    Refactor Books to hooks with a shallow merge

--- src/components/Books/index.js
+++ src/components/Books/index.js
@@ -1,26 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import books from '../../mocks/books'
 import BookList from './BookList'
 import BookFilter from './BookFilter'
 
-class Books extends React.Component {
-  constructor () {
-    super()
-    this.state = {
-      books,
-      selectedFilter: 'All',
-    }
-  }
+function Books() {
+  
+  const [state, setState] = useState({
+    books,
+    selectedFilter: 'All',
+  })
 
-  selectFilter = ( filter ) => {
-    this.setState({
+  const mergeState = partialState =>
+    setState(prevState => ({
+        ...prevState,
+        ...partialState
+    }))
+
+  const selectFilter = ( filter ) => {
+    mergeState({
       selectedFilter: filter,
-      books: filter === 'All'? books : books.filter( book => (book.category === filter) )
+      books: filter === 'All' ? books : books.filter(book => (book.category === filter))
     })
   }
 
-  render () {
-    const { books, selectedFilter } = this.state
+    
 
     return (
       <section id="books">
@@ -33,14 +36,14 @@ class Books extends React.Component {
           </div>
           <div className="row">
             <div className="col-lg-12">
-                <BookFilter selectedFilter={selectedFilter} selectFilter={this.selectFilter}/>
+                <BookFilter selectedFilter={state.selectedFilter} selectFilter={selectFilter}/>
             </div>
           </div>
-          <BookList books={books}/>
+          <BookList books={state.books}/>
         </div>
       </section>
     )
-  }
+
 }
 
 
