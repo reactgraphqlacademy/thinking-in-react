f7079486eeed33fab2c322789832d4a485c43fe1
David Leuliette <dleuliette@gmail.com>
2019-01-22 00:44:41 +0000

    Run prettier on all files for concistency

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,22 @@
 import React from 'react'
 
-const Header = (props) => (
-    <header>
-        <div className="container">
-            <div className="row">
-                <div className="col-lg-12">
-                    <img alt="" className="img-responsive logo-academy" src="/img/logo.png" />
-                    <div className="intro-text">
-                        <span className="skills">{props.title}</span>
-                    </div>
-                </div>
-            </div>
+const Header = props => (
+  <header>
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12">
+          <img
+            alt="Logo ReactJS Academy"
+            className="img-responsive logo-academy"
+            src="/img/logo.png"
+          />
+          <div className="intro-text">
+            <span className="skills">{props.title}</span>
+          </div>
         </div>
-    </header>
+      </div>
+    </div>
+  </header>
 )
 
 export default Header
--- src/components/Navigations/Menu.js
+++ src/components/Navigations/Menu.js
@@ -3,19 +3,24 @@ import React from 'react'
 import SideMenu from './Menus/SideMenu'
 import SideMenuItem from './Menus/SideMenuItem'
 
-const Menu = (props) => (
-  <SideMenu
-    isOpen={props.isOpen}
-    toggleMenu={props.toggleMenu}
-  >
+const Menu = props => (
+  <SideMenu isOpen={props.isOpen} toggleMenu={props.toggleMenu}>
     <h4>
-      Next Courses <a onClick={() => props.toggleMenu()} style={{ cursor: 'pointer' }}><i className="fa fa-close pull-right"></i></a>
+      Next Courses{' '}
+      <a onClick={() => props.toggleMenu()} style={{ cursor: 'pointer' }}>
+        <i className="fa fa-close pull-right" />
+      </a>
     </h4>
-    <SideMenuItem link="https://reactjs.academy/react-redux-training-london/">London part-time course</SideMenuItem>
-    <SideMenuItem link="https://reactjs.academy/react-redux-graphql-bootcamp-london/">London Bootcamp</SideMenuItem>
-    <SideMenuItem link="https://reactjs.academy/react-redux-graphql-bootcamp-lisbon/">Lisbon Bootcamp</SideMenuItem>
+    <SideMenuItem link="https://reactjs.academy/react-redux-training-london/">
+      London part-time course
+    </SideMenuItem>
+    <SideMenuItem link="https://reactjs.academy/react-redux-graphql-bootcamp-london/">
+      London Bootcamp
+    </SideMenuItem>
+    <SideMenuItem link="https://reactjs.academy/react-redux-graphql-bootcamp-lisbon/">
+      Lisbon Bootcamp
+    </SideMenuItem>
     <SideMenuItem>GraphQL (comming soon)</SideMenuItem>
-
   </SideMenu>
 )
 
